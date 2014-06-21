8243feb86f87445aef66b35721895ffa3c53289b
invaa <alex@zamkovyi.name>
2014-06-21 12:38:03 +0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace QSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input array elements separated with spaces:");
+            string inputString = "";
+
+            inputString = Console.ReadLine();
+
+           
+            Debug.Assert(inputString != null, "inputString != null");
+            string[] array = inputString.Split(' ');
+
+            ArrayList arrayToSort = new ArrayList();
+            foreach (var el in array)
+            {
+                arrayToSort.Add(Convert.ToInt32(el));
+            }
+
+            QSort.Sorter.QSort(ref arrayToSort);
+
+            foreach (var el in arrayToSort)
+            {
+               Console.Write(el.ToString() + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Sorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QSort
+{
+    public static class Sorter
+    {
+        public static void QSort(ref ArrayList array)
+        {
+
+            QSort(ref array, 0, array.Count - 1);
+        }
+        private static void QSort(ref ArrayList array, int l, int r)
+        {
+            int i = l;
+            int j = r;
+            Object m = array[r - (r - l)/2];
+
+            while (i <= j)
+            {
+                while (((IComparable)array[i]).CompareTo(m) < 0) i++;
+                while (((IComparable)array[j]).CompareTo(m) > 0) j--;
+
+                if (i <= j) { Swap(ref array, i, j); i++; j--; };
+            }
+
+            if (i < r) QSort(ref array, i, r);
+            if (j > l) QSort(ref array, l, j);
+
+        }
+
+        private static void Swap(ref ArrayList array, int i, int j)
+        {
+            Object c = array[i];
+            array[i] = array[j];
+            array[j] = c;
+        }
+     }
+}
